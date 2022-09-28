22ea890a6b2cf0f0c08d8ba949d44892c44ef4c6
Michael Portelas <michael.portelas@gmail.com>
2022-09-28 19:02:21 +0200

    test message de bienvenue sur page Home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,6 +1,19 @@
+import { useSelector } from "react-redux"
+import { selectCache } from "../../utils/selectors"
 
 function Home(){
-    
+    const cache = useSelector(selectCache)
+
+    if (cache) {
+        return (
+            <div>
+                <div>Bienvenue {cache.pseudo}</div>
+            </div>
+            
+        );
+        
+    }
+
     return (
         <div>Hello world</div>
     )
