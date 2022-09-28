412902dab05f3ec9c27a0f87390dea811acabede
Michael Portelas <michael.portelas@gmail.com>
2022-09-28 18:50:16 +0200

    Mise en place de la logique Creation utilisateur

--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,52 +1,89 @@
-import { selectUser } from "../../utils/selectors";
-import { useDispatch, useSelector } from 'react-redux'
-import { createUser } from "../../features/user";
-import { useEffect } from "react";
+import { useRef } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCache } from "../../utils/selectors";
+import { setCache } from "../../features/users.slice";
 
 const SignUp = () => {
-    const user = useSelector(selectUser)
-
-    const HandleSubmit = async e => {
+    const formRef = useRef();
+    const inputPseudo = useRef();
+    const inputEmail = useRef();
+    const inputPassword = useRef();
+    const dispatch = useDispatch();
+    
+    const cache = useSelector(selectCache);
+    
+    const HandleSubmit = (e) => {
         
-        const dispatch = useDispatch()
-
-        e.preventDefault();      
+        e.preventDefault();
         
-        useEffect(() => {
-            dispatch(createUser)
-        }, [dispatch])
+        const data = {
+            pseudo: inputPseudo.current.value,
+            email:  inputEmail.current.value,
+            password:  inputPassword.current.value,
+        }
 
+        axios.post('http://localhost:3000/api/auth/signup', data)
+        .then((res) => {
+            console.log(res.data.message);
+
+            formRef.current.reset();
+
+            // On Connecte le nouveau user et on mets en cache la session
+            axios.post('http://localhost:3000/api/auth/login', data)
+            .then((res) => {
+                
+                dispatch(setCache(res.data));
+
+                localStorage.setItem('user', JSON.stringify(res.data));
+
+                //on renvoie l'utilisateur vers la page d'accueil
+                window.location.href='/';
+
+            })
+            .catch((error) => {
+                console.log(error)
+                return <span>Oupssss!! il y a un problème</span>
+            })
+        })
+        .catch((error) => {
+            console.log(error)
+            return <span>Oupssss!! il y a un problème</span>
+        })
+        
     }
 
+    if (cache) {
+        return (
+            <div>
+                <div>{cache.pseudo} is loggged in </div>
+            </div>
+            
+        );
+        
+    }
      
-    // if there's no user, show the login form
     return (
-        <form onSubmit={HandleSubmit}>
-            <label htmlFor="pseudo">Pseudo : </label>
+        <form onSubmit={(e) => HandleSubmit(e)} ref={formRef}>
             <input
-            type="text"
-            value={user.pseudo}
-            placeholder="enter a username"
-            onChange={({ target }) => user.pseudo = target.value}
+                type="text"
+                placeholder="enter a username"
+                ref={inputPseudo}
             />
             
             <div>
-            <label htmlFor="mail">Mail : </label>
             <input
-            type="text"
-            value={user.email}
-            placeholder="enter a username"
-            onChange={({ target }) => user.email = target.value}
+                type="email"
+                placeholder="enter a username"
+                ref={inputEmail}
             />
             </div>
             
             <div>
-            <label htmlFor="password">password: </label>
             <input
                 type="password"
-                value={user.password}
                 placeholder="enter a password"
-                onChange={({ target }) => user.password = target.value }
+                ref={inputPassword}
             />
             </div>
             <button type="submit">Register</button>
