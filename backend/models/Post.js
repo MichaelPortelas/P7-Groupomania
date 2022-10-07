33c095f762aedc7f4bb5583a9bd2cf46654cb364
Michael Portelas <michael.portelas@gmail.com>
2022-10-07 13:00:17 +0200

    [Back] ajout pseudo dans le model post

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const postSchema = mongoose.Schema({
     userId: { type: String, required: true },
+    pseudo: { type: String, required: true },
     message: { type: String, required: true },
     imageUrl: { type: String},
     date: { type: Date, required: true },
