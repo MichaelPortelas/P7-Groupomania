eb63fefcbaf04227d023022e34e2c303e004cda5
Michael Portelas <michael.portelas@gmail.com>
2022-10-14 10:25:10 +0200

    [Back] correction indentation

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -24,40 +24,40 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   };
   
-  exports.login = (req, res, next) => {
+exports.login = (req, res, next) => {
     // on recheche l'user via sont email avec la methode findOne
-     User.findOne({ email: req.body.email })
-         .then(user => {
-            // si il n'existe pas on renvoie une erreur générique WCAG
-             if (!user) {
-                 return res.status(401).json({ error: 'Identification incorrect !' });
-             }
+    User.findOne({ email: req.body.email })
+    .then(user => {
+    // si il n'existe pas on renvoie une erreur générique WCAG
+        if (!user) {
+            return res.status(401).json({ error: 'Identification incorrect !' });
+        }
 
-             // on compare le mot de passe envoyé a celui de la base
-             bcrypt.compare(req.body.password, user.password)
-                .then(valid => {
-                    // si il n'est pas valide on renvoie une erreur générique WCAG
-                    if (!valid) {
-                         return res.status(401).json({ error: 'Identification incorrect !' });
-                    }
-                    
-                    // on renvois l'id, le pseudo, le status admin ainsi qu'un token d'authentification.
-                    res.status(200).json({
-                        userId: user._id,
-                        pseudo: user.pseudo,
-                        admin: user.admin,
-                        token: jwt.sign(
-                        { 
-                            userId: user._id,
-                            pseudo: user.pseudo,
-                            admin: user.admin,                                 
-                        },
-                            'KSkCQocJLzlx1-UkEaOtjKcrH0oP5C1bWadvVAqOeNHr7yL7eTd6GX80DvQ1cFh7FVZQ',
-                            { expiresIn: '24h' }
-                        ),
-                    });
-                 })
-                 .catch(error => res.status(500).json({ error }));
-         })
-         .catch(error => res.status(500).json({ error }));
-  };
+        // on compare le mot de passe envoyé a celui de la base
+        bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+            // si il n'est pas valide on renvoie une erreur générique WCAG
+            if (!valid) {
+                    return res.status(401).json({ error: 'Identification incorrect !' });
+            }
+            
+            // on renvois l'id, le pseudo, le status admin ainsi qu'un token d'authentification.
+            res.status(200).json({
+                userId: user._id,
+                pseudo: user.pseudo,
+                admin: user.admin,
+                token: jwt.sign(
+                { 
+                    userId: user._id,
+                    pseudo: user.pseudo,
+                    admin: user.admin,                                 
+                },
+                    'KSkCQocJLzlx1-UkEaOtjKcrH0oP5C1bWadvVAqOeNHr7yL7eTd6GX80DvQ1cFh7FVZQ',
+                    { expiresIn: '24h' }
+                ),
+            });
+            })
+            .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
